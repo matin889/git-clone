addda90a1946f90a807ecd4c79592fbbb483601c
matin889 <bornlitematin@gmail.com>
2023-10-31 22:55:57 +0100

    data updated

--- src/basicZodUsage.ts
+++ src/basicZodUsage.ts
@@ -1,10 +1,10 @@
-import { z } from "Zod"
+import { z } from "Zod";
 
 // Skapar ett schema för en sträng
-const mySchema = z.string()
+const mySchema = z.string();
 
-// Parsing 
-//mySchema.parse("sandra") 
+// Parsing
+//mySchema.parse("sandra")
 //mySchema.parse(3143) // Kastar ett Zod-fel
 
 // "safe"-parsing - kastar inte ett fel om valideringen misslyckas
@@ -12,11 +12,11 @@ const mySchema = z.string()
 //console.log(mySchema.safeParse(12344))
 
 const userSchema = z.object({
-    name: z.string(),
-    age: z.number()
-})
+  name: z.string(),
+  age: z.number(),
+});
 
-const response = userSchema.safeParse({name: "Sandra", age: "18"});
-console.log(response)
+const response = userSchema.safeParse({ name: "Matin", age: "40" });
+console.log(response);
 
-type User = z.infer<typeof userSchema>
+type User = z.infer<typeof userSchema>;
